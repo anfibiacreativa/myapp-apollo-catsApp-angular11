d4038718e43d0445f6ac6518c9e9ea6db1294eeb
Natalia Venditto <nvenditto@nvenditto-MBP.local>
2022-06-16 15:56:33 +0200

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,8 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-module.exports = {
-
-  optimization: {
-    // Only needed to bypass a temporary bug
-    runtimeChunk: false
-  },
-  library: { type: "module" },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "microfe1",
-      remotes: {
-      },
-      library: { type: "var", name: "microfe1" },
-      filename: "remoteEntry.js",
-      exposes: {
-      }, 
-    })
-  ],
-};
-
-
-
-
-
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
   name: 'microfe1',
-  output: {
-    publicPath: "http://localhost:4200/",
-    uniqueName: "microfe1"
-  },
-  exposes: {
-    './Module': './projects/mfe1/src/app/flights/flights.module.ts',
-  },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
